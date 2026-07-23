411641935b584dfee3c302e32a4e7e3eb311a199
Satya Prakash <satyaprakashdh3134@gmail.com>
2026-07-23 19:34:04 +0530

    feat: add audio assets and data, configure EAS, and update native build scripts and permissions

--- data/song.ts
+++ data/song.ts
@@ -8,20 +8,38 @@ export interface Song {
 export const songs: Song[] = [
   {
     id: "1",
-    title: "Song One",
-    artist: "Local Track",
+    title: "Hare Krishna",
+    artist: "jubil notial",
     file: require("../assets/song/song1.mp3"),
   },
   {
     id: "2",
-    title: "Song Two",
-    artist: "Synth Melodies",
-    file: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3",
+    title: "Ijazat",
+    artist: "arijit singh",
+    file: require("../assets/song/song2.mp3"),
   },
   {
     id: "3",
-    title: "Song Three",
-    artist: "Piano Chill",
-    file: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3",
+    title: "Wajah tum ho",
+    artist: "zareen khan",
+    file: require("../assets/song/song3.mp3"),
+  },
+  {
+    id: "4",
+    title: "Ghara Hua",
+    artist: "Arijit Singh",
+    file: require("../assets/song/song4.mp3"),
+  },
+  {
+    id: "5",
+    title: "tum ho to",
+    artist: "Unknown",
+    file: require("../assets/song/song5.mp3"),
+  },
+  {
+    id: "6",
+    title: "finding her",
+    artist: "Kushagra ",
+    file: require("../assets/song/song6.mp3"),
   },
 ];
